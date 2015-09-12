df00a228c5e22cb1a411b51b8e6b0e91a5bb729c
baydindima <QCtEes=9,29)vzW>
2015-09-12 16:48:03 +0300

    Add JUnit - for testing Add JSqlParser - for SQL parse

--- src/main/java/ru/spbau/mit/ConsoleController.java
+++ src/main/java/ru/spbau/mit/ConsoleController.java
@@ -1,5 +1,9 @@
 package ru.spbau.mit;
 
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,20 +17,25 @@ public class ConsoleController {
 
     DatabaseProperties databaseProperties;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, JSQLParserException {
         System.out.println("Tiny Database command line tool\n");
         DatabaseProperties databaseProperties = DatabaseProperties.setOptions(args);
 
-        System.out.println("\tType 2 times ENTER to execute any SQL command.");
+        System.out.println("Type 2 times ENTER to execute any SQL command.");
 
         StringBuilder command = new StringBuilder();
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             String line = input.readLine();
             if(line.length() == 0 && command.length() > 0){
-                break;
-                //execute command
+                Statement statement = CCJSqlParserUtil.parse(command.toString());
+                System.out.println(statement);
+                command = new StringBuilder();
             }
+
+
+            if (line.toLowerCase().trim().equals("quit")) break;
+
             command.append(line).append('\n');
         }
     }
