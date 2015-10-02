d4b3df3a1cb4cfd38c83afabe463ce523a3878d8
edgarzhavoronkov <edzhavoronkov@gmail.com>
2015-10-02 14:38:29 +0300

    Add getFromPage method

--- src/main/java/ru/spbau/mit/meta/DataType.java
+++ src/main/java/ru/spbau/mit/meta/DataType.java
@@ -1,6 +1,10 @@
 package ru.spbau.mit.meta;
 import ru.spbau.mit.memory.Page;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 /**
  * Created by John on 9/12/2015.
  */
@@ -10,11 +14,21 @@ public enum DataType {
         public void putInPage(Object o, Page page) {
             page.getByteBuffer().putInt((Integer)o);
         }
+
+        @Override
+        public Object getFromPage(Page page) {
+            return page.getByteBuffer().getInt();
+        }
     },
     DOUBLE (Double.class, Double.SIZE) {
         @Override
         public void putInPage(Object o, Page page) {
             page.getByteBuffer().putDouble((Double)o);
+        }
+
+        @Override
+        public Object getFromPage(Page page) {
+            return page.getByteBuffer().getDouble();
         }
     },
     VARCHAR (String.class, Constants.VARCHAR_MAXIMUM_SIZE) {
@@ -22,6 +36,22 @@ public enum DataType {
         public void putInPage(Object o, Page page) {
             page.getByteBuffer().put((byte) ((String)o).length());
             page.getByteBuffer().put(((String)o).getBytes());
+        }
+
+        @Override
+        public Object getFromPage(Page page) {
+            byte[] buffer = new byte[Constants.VARCHAR_MAXIMUM_SIZE];
+            page.getByteBuffer().get(buffer);
+            byte length = buffer[0];
+            byte[] stringToReturn = new byte[length];
+            System.arraycopy(buffer, 1, stringToReturn, 1, length + 1 - 1);
+            ByteArrayInputStream stream = new ByteArrayInputStream(stringToReturn);
+            try (ObjectInputStream objectInputStream = new ObjectInputStream(stream)) {
+                return objectInputStream.readObject();
+            } catch (ClassNotFoundException | IOException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     };
 
@@ -46,4 +76,6 @@ public enum DataType {
     }
 
     public abstract void putInPage(Object o, Page page);
+
+    public abstract Object getFromPage(Page page);
 }
