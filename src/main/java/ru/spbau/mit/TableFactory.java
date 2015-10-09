398d26b4637cf88bda61ff868ef87a0998001ea4
baydindima <QCtEes=9,29)vzW>
2015-10-09 16:10:31 +0300

    TableFactory

--- src/main/java/ru/spbau/mit/TableFactory.java
+++ src/main/java/ru/spbau/mit/TableFactory.java
@@ -1,12 +1,12 @@
 package ru.spbau.mit;
 
 import com.google.gson.Gson;
+import ru.spbau.mit.meta.Column;
+import ru.spbau.mit.meta.DataType;
 import ru.spbau.mit.meta.Table;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,41 +15,28 @@ import java.util.Map;
  */
 final public class TableFactory {
 
-    static private Map<String, Table> tableMap;
-
-//    public static void main(String[] args) throws FileNotFoundException {
-//        Table test_table = getTable("test_table");
-//        System.out.println(test_table);
-//    }
-
-//    public static void main(String[] args) {
-//        ArrayList<Column> columns = new ArrayList<>();
-//        columns.add(new Column("col1", DataType.DOUBLE));
-//        columns.add(new Column("col2", DataType.VARCHAR));
-//        columns.add(new Column("col3", DataType.INTEGER));
-//        Table table = new Table("tableName", columns);
-//        String json = new Gson().toJson(table);
-//        System.out.println(json);
-//    }
+    static private Map<String, Table> tableMap = new HashMap<>();
 
     static public Table getTable(String tableName) throws FileNotFoundException {
-        if (tableMap == null) {
-            tableMap = new HashMap<>();
-        }
         Table table = tableMap.get(tableName);
         if (table == null) {
             String folder = PropertiesManager.getProperties().getProperty("meta_folder");
-            //todo filepath add folder
-            String tablePath = tableName + ".json";
+            String tablePath = folder + "\\" + tableName + ".json";
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(tablePath)));
             table = new Gson().fromJson(reader, Table.class);
         }
         return table;
     }
 
-    static public void addTable(Table table) {
-        //todo filepath add folder
+    static public void addTable(Table table) throws IOException {
+        tableMap.put(table.getName(), table);
 
+        String folder = PropertiesManager.getProperties().getProperty("meta_folder");
+        String tablePath = folder + "\\" + table.getName() + ".json";
 
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(tablePath), "utf-8"))) {
+            writer.write(new Gson().toJson(table));
+        }
     }
 }
--- src/test/java/mit/spbau/mit/TableFactoryTest.java
+++ src/test/java/mit/spbau/mit/TableFactoryTest.java
@@ -9,10 +9,26 @@ import java.io.FileNotFoundException;
  * Created by John on 10/2/2015.
  */
 public class TableFactoryTest {
-    @Test
-    public void testTableCreate() throws FileNotFoundException {
+//    @Test
+//    public void testTableCreate() throws FileNotFoundException {
+//
+//        TableFactory.getTable("test_table");
+//
+//    }
 
-        TableFactory.getTable("test_table");
 
-    }
+    //    public static void main(String[] args) throws FileNotFoundException {
+//        Table test_table = getTable("tableName");
+//        System.out.println(test_table);
+//    }
+
+//    public static void main(String[] args) throws IOException {
+//        ArrayList<Column> columns = new ArrayList<>();
+//        columns.add(new Column("col1", DataType.DOUBLE));
+//        columns.add(new Column("col2", DataType.VARCHAR));
+//        columns.add(new Column("col3", DataType.INTEGER));
+//        Table table = new Table("tableName", columns);
+////        String json = new Gson().toJson(table);
+//        addTable(table);
+//    }
 }
