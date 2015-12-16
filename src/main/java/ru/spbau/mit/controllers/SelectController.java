9c31e124ddf3cd36419f3b1684e340d759465725
baydindima <QCtEes=9,29)vzW>
2015-12-16 12:29:30 +0300

    SelectController index

--- src/main/java/ru/spbau/mit/controllers/SelectController.java
+++ src/main/java/ru/spbau/mit/controllers/SelectController.java
@@ -1,6 +1,10 @@
 package ru.spbau.mit.controllers;
 
 import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.LongValue;
+import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
+import net.sf.jsqlparser.expression.operators.relational.GreaterThan;
+import net.sf.jsqlparser.expression.operators.relational.GreaterThanEquals;
 import net.sf.jsqlparser.expression.operators.relational.OldOracleJoinBinaryExpression;
 import net.sf.jsqlparser.statement.Statement;
 import net.sf.jsqlparser.statement.select.AllColumns;
@@ -24,6 +28,7 @@ import java.io.IOException;
  */
 public class SelectController implements QueryController {
     private final BufferManager bufferManager;
+
     private SelectController(BufferManager bufferManager) {
         this.bufferManager = bufferManager;
     }
@@ -35,51 +40,24 @@ public class SelectController implements QueryController {
     @Override
     public QueryResponse process(Statement statement) throws IOException {
         try {
-            if(!(statement instanceof Select)){
+            if (!(statement instanceof Select)) {
                 throw new SQLParserException("Not a select statement: ", statement);
             }
-            PlainSelect plainSelect = (PlainSelect)(((Select) statement).getSelectBody());
+            PlainSelect plainSelect = (PlainSelect) (((Select) statement).getSelectBody());
             Expression whereExpression = plainSelect.getWhere();
-            String tableName = ((net.sf.jsqlparser.schema.Table)plainSelect.getFromItem()).getName();
+            String tableName = ((net.sf.jsqlparser.schema.Table) plainSelect.getFromItem()).getName();
             Table table = TableFactory.getTable(tableName);
-            if(plainSelect.getSelectItems().get(0) instanceof AllColumns) {
+            if (plainSelect.getSelectItems().get(0) instanceof AllColumns) {
                 if (whereExpression != null) {
-                    String columnName = ((net.sf.jsqlparser.schema.Column) ((OldOracleJoinBinaryExpression) whereExpression).getLeftExpression()).getColumnName();
-                    Column column = null;
-                    if (table.getIndexedColumns().containsKey(columnName)) {
-                        for (Column column1 : table.getColumns()) {
-                            if (column1.getName().equals(columnName)) {
-                                column = column1;
-                                break;
-                            }
-                        }
-                    }
 
-                    Cursor cursorIndex = null;
-                    if (column != null) {
-                        cursorIndex = new TreeIndexCursor(bufferManager, table, column, 0);
-                    }
-
-
-
-                    Cursor innerCursor = new FullScanCursor(bufferManager, table, table.getFirstPageId(), 0);
+                    Cursor indexCursor = getIndexCursor(whereExpression, table);
                     Cursor cursor;
-                    if (cursorIndex != null) {
-                        cursor = new WhereCursor(cursorIndex, whereExpression);
+                    if (indexCursor != null) {
+                        cursor = new WhereCursor(indexCursor, whereExpression);
                     } else {
+                        Cursor innerCursor = new FullScanCursor(bufferManager, table, table.getFirstPageId(), 0);
                         cursor = new WhereCursor(innerCursor, whereExpression);
                     }
-//                    Cursor cursor = new WhereCursor(innerCursor, whereExpression);
-//                    ((Column) ((EqualsTo) whereExpression).getLeftExpression()).getColumnName()
-//                    ((Column) ((OldOracleJoinBinaryExpression) whereExpression).getLeftExpression()).getColumnName()
-//                    OldOracleJoinBinaryExpression
-//                    OldOracleJoinBinaryExpression
-                    //select * from t1 where id = 1
-
-//                    Column column = new Column(columnName);
-
-
-
 
                     return new QueryResponse(QueryResponse.Status.OK, cursor);
                 }
@@ -92,10 +70,38 @@ public class SelectController implements QueryController {
         }
     }
 
-    private Column getIndexColumn() {
+    private Cursor getIndexCursor(Expression whereExpression, Table table) throws SQLParserException, IOException {
+        OldOracleJoinBinaryExpression binaryExpression = (OldOracleJoinBinaryExpression) whereExpression;
+        net.sf.jsqlparser.schema.Column leftExpression = (net.sf.jsqlparser.schema.Column) (binaryExpression).getLeftExpression();
+
+        if (!(binaryExpression.getRightExpression() instanceof LongValue)) {
+            throw new SQLParserException("Index permit only for INT columns!");
+        }
+
+        Integer value = new Long(((LongValue) binaryExpression.getRightExpression()).getValue()).intValue();
+        String columnName = leftExpression.getColumnName();
+
+        Column column = null;
+        if (table.getIndexedColumns().containsKey(columnName)) {
+            for (Column column1 : table.getColumns()) {
+                if (column1.getName().equals(columnName)) {
+                    column = column1;
+                    break;
+                }
+            }
+        }
+
+        if (column != null) {
+            if (whereExpression instanceof GreaterThanEquals ||
+                    whereExpression instanceof GreaterThan ||
+                    whereExpression instanceof EqualsTo) {
+                return new TreeIndexCursor(bufferManager, table, column, value - 1);
+            } else {
+                return new TreeIndexCursor(bufferManager, table, column, Integer.MIN_VALUE);
+            }
+        }
+
         return null;
     }
-
-    ;
 
 }
